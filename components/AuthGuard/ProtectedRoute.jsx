ab5ce768ba9cb2b840992f2d1a89b83db1104449
rabiul155 <siyamict@gamil.com>
2024-05-06 15:08:50 +0600

    handle router

--- components/AuthGuard/ProtectedRoute.jsx
+++ components/AuthGuard/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Loading from "../Loading/Loading";
 import { useRouter } from "next/navigation";
@@ -8,12 +8,17 @@ import { useRouter } from "next/navigation";
 function ProtectedRoute({ children }) {
   const auth = useSelector((state) => state.auth);
   const { loading, user } = auth;
-
   const router = useRouter();
+
+  useEffect(() => {
+    if (!user.email) {
+      router.replace("/login");
+    }
+  }, [user.email]);
 
   if (loading) return <Loading></Loading>;
 
-  return user.email ? children : router.push("/login");
+  return user.email ? children : null;
 }
 
 export default ProtectedRoute;
